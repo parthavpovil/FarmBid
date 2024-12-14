bd07cff704940dc0dadef3cff578466df6d33abf
Adwaith Jayasankar <kichu31084@gmail.com>
2024-12-14 21:48:40 +0530

    Update: Added financial page from 8mani

---
+++ lib/screens/financial_assistance_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FinancialAssistancePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Financial Assistance'),
+      ),
+      body: const Center(
+        child: Text('Coming Soon'),
+      ),
+    );
+  }
+}
